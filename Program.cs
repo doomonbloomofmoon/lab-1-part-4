2c84d8ac94358c0a3955bea13241a7d9e34e0345
DOdGe <dimacernovolov@gmail.com>
2023-10-08 19:44:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_1_Part_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
